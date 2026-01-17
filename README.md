88a78bacc2174d64fbc233a2e85041efb1cafb84
ingmar <ingmania@protonmail.com>
2026-01-17 09:47:10 -0600

    code and readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# Color theory
+
+Create color palettes from a given initial color.
+
+## args
+
+You can run it with no args:
+
+`racket -tm color-theory.rkt`
+
+### fidelity
+
+Determines the number of colors created. Note that fidelity is not the actual number of colors, but the number of iterations used to create the colors. Thus fidelity is exponential in the number of colors created.
+
+If you must know precisely, the number of colors is:
+
+> `(* 3 (expt 2 (- fidelity 1)))`
+
+Run `racket -tm color-theory.rkt 5` for 48 colors. 5 is the default fidelity.
+
+### Starting colors
+
+You can specify one or two colors in hexadecimal which will be used as input for the colors to generate. Check out the color palette for *Burgundy*:
+
+`racket -tm color-theory.rkt 5 800020`
+
+## Dependencies
+
+color-theory requieres [raylib](https://www.raylib.com/). Install the wrapper with `raco setup raylib`.
---
+++ color-theory.rkt
@@ -0,0 +1,207 @@
+#lang racket
+
+(provide main
+         dim/brighten)
+
+(require racket/fixnum
+         raylib/generated/unsafe)
+
+(define (main (fidelity "5") . args)
+  (set-colors (string->number fidelity) args)
+  (open)
+  (let loop ((nix null))
+    (handle-input)
+    (draw)
+    (if (WindowShouldClose)
+        (CloseWindow)
+        (loop nix))))
+
+(define width 640)
+(define height 480)
+
+(define (open)
+  (InitWindow width height "Colors")
+  (SetTargetFPS 60))
+
+(define (draw)
+  (BeginDrawing)
+  (ClearBackground WHITE)
+  (draw-colors width height)
+  (EndDrawing))
+
+(define (draw-colors width height)
+  (let ((rects (rectangles width height)))
+    (for* ((row rects)
+           (cell row))
+      (DrawRectangleRec (car cell) (cadr cell)))))
+
+(define colors (list (list WHITE)))
+
+(define (rectangles width height)
+  (let ((h (/ height (length colors))))
+    (for/list ((y (length colors))
+               (row colors))
+      (let ((w (/ width (length row))))
+        (for/list ((x (length row))
+                   (color row))
+          (list (make-Rectangle (exact->inexact (* x w))
+                                (exact->inexact (* y h))
+                                (exact->inexact w)
+                                (exact->inexact h))
+                color))))))
+
+(define (handle-input)
+  (when (IsMouseButtonPressed MOUSE_BUTTON_LEFT)
+    (let ((cell (for/or ((row (rectangles width height)))
+                  (findf (lambda (item)
+                           (CheckCollisionPointRec (GetMousePosition) (car item)))
+                         row))))
+      (display-color (cadr cell)))))
+
+(define (display-color color)
+  (let ((fmt (lambda (s) (~r #:base 16 #:min-width 2 #:pad-string "0" s))))
+    (displayln (format "~a~a~a"
+                       (fmt (Color-r color))
+                       (fmt (Color-g color))
+                       (fmt (Color-b color))))))
+
+(define (set-colors fidelity color-list)
+  (case (length color-list)
+    ((1) (let ((color (decode-color-string (car color-list))))
+           (set! colors (color-grid (opposites color) fidelity))))
+    ((2) (let ((color1 (decode-color-string (car color-list)))
+               (color2 (decode-color-string (cadr color-list))))
+           (let ((start-colors (opposite color1 color2)))
+             (set! colors (color-grid start-colors fidelity)))))
+    (else (let ((color (make-Color #xFF #x00 #x00 #xFF)))
+            (set! colors (color-grid (opposites color) fidelity))))))
+
+(define (decode-color-string color-string)
+  (let ((r (string->number (substring color-string 0 2) 16))
+        (g (string->number (substring color-string 2 4) 16))
+        (b (string->number (substring color-string 4 6) 16)))
+    (make-Color r g b #xFF)))
+
+(define (color->values color)
+  (values (Color-r color) (Color-g color) (Color-b color))) 
+
+(define (color->list color)
+  (list (Color-r color) (Color-g color) (Color-b color))) 
+
+(define (Color+ c1 c2)
+  (color+ (color->list c1) (color->list c2)))
+
+(define (color+ lst1 lst2)
+  (let* ((r (+ (first lst1) (first lst2)))
+         (g (+ (second lst1) (second lst2)))
+         (b (+ (third lst1) (third lst2)))
+         (max1 (apply max lst1))
+         (max2 (apply max lst2)))
+    (dim-color r g b (/ (+ max1 max2) 2))))
+
+(define (dim-color r g b brightness)
+  (let ((factor (/ (max r g b) brightness)))
+    (make-Color (min #xFF (truncate (/ r factor)))
+                (min #xFF (truncate (/ g factor)))
+                (min #xFF (truncate (/ b factor)))
+                #xFF)))
+
+(define (dim/brighten color brightness)
+  (let*-values (((color) (decode-color-string color))
+                ((r g b) (color->values color)))
+    (display-color (dim-color r g b brightness))))
+
+(define (opposites color)
+  (let-values (((r g b) (color->values color)))
+    (list color
+          (make-Color b r g #xFF)
+          (make-Color g b r #xFF))))
+
+(define (shift-dir index target)
+  (let ((dir (- target index)))
+    (case dir
+      ((2) -1)
+      ((-2) 1)
+      (else dir))))
+
+(define (list-shift lst dir)
+  (case dir
+    ((1) (cons (last lst) (drop-right lst 1)))
+    ((-1) (append (drop lst 1) (take lst 1)))))
+
+(define (opposite c1 c2)
+  (let-values (((r1 g1 b1) (color->values c1))
+               ((r2 g2 b2) (color->values c2)))
+    (let* ((lst1 (list r1 g1 b1))
+           (lst2 (list r2 g2 b2))
+           (max1 (max r1 g1 b1))
+           (max2 (max r2 g2 b2))
+           (index1 (index-of lst1 max1))
+           (index2 (index-of lst2 max2))
+           (target (car (remove* (list index1 index2) '(0 1 2)))))
+      (values (list c1
+                    c2 
+                    (color+ (list-shift lst1 (shift-dir index1 target))
+                            (list-shift lst2 (shift-dir index2 target))))))))
+
+(define (new-color-row previous-color-row)
+  (let loop ((remaining previous-color-row)
+             (result empty))
+    (if (null? remaining)
+        (reverse result)
+        (loop (rest remaining)
+              (let ((other (if (= (length remaining) 1)
+                               (first previous-color-row)
+                               (second remaining))))
+                (cons (Color+ (first remaining) other)
+                      (cons (first remaining) result)))))))
+
+(define (color-pyramid first-color-row number-of-rows)
+  (let loop ((result (list first-color-row)))
+    (if (= (length result) number-of-rows)
+        (begin
+          (displayln (format "number of colors: ~a" (length (first result))))
+          (reverse result))
+        (loop (cons (new-color-row (first result)) result)))))
+
+(define (py1 color rows)
+  (let-values (((r g b) (color->values color)))
+    (set! colors (color-pyramid (opposites color) rows))
+    (draw)))
+
+(define (py2 color-list rows)
+  (let ((complemented-colors (opposite (first color-list)
+                                       (second color-list))))
+    (set! colors (color-pyramid complemented-colors rows))
+    (draw)))
+
+(define (factors number)
+  (let ((x number)
+        (y 1))
+    (for* ((a number)
+           (b (in-range number 0 -1))
+           #:when (< a b))
+      (when (= (* a b) number)
+        (set! x b)
+        (set! y a)))
+    (values x y)))
+
+(define (color-grid start-colors fidelity)
+  (let ((colors (last (color-pyramid start-colors fidelity))))
+    (let-values (((columns rows) (factors (length colors))))
+      (let loop ((result (list (take colors columns)))
+                 (remaining-colors (drop colors columns)))
+        (if (empty? remaining-colors)
+            (reverse result)
+            (loop (cons (take remaining-colors columns) result)
+                  (drop remaining-colors columns)))))))
+
+(define (gi1 color fidelity)
+  (set! colors (color-grid (opposites color) fidelity))
+  (draw))
+
+(define (gi2 color-list fidelity)
+  (let ((complemented-colors (opposite (first color-list)
+                                       (second color-list))))
+    (set! colors (color-grid complemented-colors fidelity))
+    (draw)))
